787e3b916f57eb22f63e37d45b00340e2b926b62
18robinsonproduction <46098316+18robinsonproduction@users.noreply.github.com>
2019-05-13 13:18:47 +0700

    Update MyFirebaseInstanceIDService
    FirebaseInstanceIdService deprecated => In favour of overriding onNewToken in FirebaseMessagingService

--- src/android/MyFirebaseInstanceIDService.java
+++ src/android/MyFirebaseInstanceIDService.java
@@ -3,23 +3,18 @@ package com.gae.scaffolder.plugin;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
-/**
- * Created by Felipe Echanique on 08/06/2016.
- */
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+public class MyFirebaseInstanceIDService extends FirebaseMessagingService  {
 
     private static final String TAG = "FCMPlugin";
 
     @Override
-    public void onTokenRefresh(){
+    public void onNewToken(String token) { //Added onNewToken method
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
-		FCMPlugin.sendTokenRefresh( refreshedToken );
 
-        // TODO: Implement this method to send any registration to your app's servers.
-        //sendRegistrationToServer(refreshedToken);
+        FCMPlugin.sendTokenRefresh( refreshedToken );
     }
 }
